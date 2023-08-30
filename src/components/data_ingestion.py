56a652e9f1b33d16beb85bae9dc6a7fdff3813b2
Shubham_Katara <shubhamkatara@arthmate.com>
2023-08-30 08:58:01 +0530

    Data Ingestion 1

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -26,7 +26,7 @@ class DataIngestion:
             
             # storing data into directory
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
-            df.to_csv(self.ingestion_config.train_data_path,index=False)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False)
 
             logging.info("Train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
